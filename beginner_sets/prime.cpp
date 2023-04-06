f0a36336b459ab376358415a085daebd26aebfaa
shankerabhigyan <shankerabhigyan@gmail.com>
2023-04-06 15:33:56 +0530

    new files

---
+++ beginner_sets/prime.cpp
@@ -0,0 +1,36 @@
+//find out the largest prime factor of a x
+#include <iostream>
+using namespace std;
+
+int isPrime(int n){
+    // from 2 to square root of n
+    for (int i = 2; i*i <= n; i++){
+        if (n % i == 0){
+            return false;
+        }
+    }
+    return true;
+}
+
+int PrimeFactors(int n){
+    //create linked list of prime factors of n
+    
+    // from 2 to square root of n
+    for (int i = 2; i*i <= n; i++){
+        if (n % i == 0){
+            if (isPrime(i)){
+                cout << i << endl;
+            }
+        }
+    }
+    return true;
+}
+
+int main(){
+    int n=0;
+    cin >> n;
+    if (isPrime(n)){
+        cout << n << endl;
+    }
+    PrimeFactors(n);
+}
