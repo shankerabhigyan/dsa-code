8c8f88a8481926277c0e2655c22affb5c59a69f7
shankerabhigyan <shankerabhigyan@gmail.com>
2023-04-16 13:14:32 +0530

    updates

--- basics/array.cpp
+++ basics/array.cpp
@@ -2,11 +2,19 @@
 #include<math.h>
 using namespace std;
 
+void printArray(int array[], int size){
+    for(int i=0; i<size; i++){
+        cout << array[i] << " ";
+    }
+    cout << endl;
+}
+
 int main(){
     
-    int arr[17];
-    fill_n(arr, 17, 1);
-    cout << arr[6] << endl;
-    
+    int arr[17] = {0};
+    fill_n(arr, 17, 5);
+    //cout << sizeof(arr)/sizeof(int) << endl;
+    //cout << arr[6] << endl;
+    printArray(arr, sizeof(arr)/sizeof(int));
     return 0;
 }
