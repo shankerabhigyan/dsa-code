b1c73a9d523f4fbfdcdc8a21eff2cdb0f038a80d
shankerabhigyan <shankerabhigyan@gmail.com>
2023-04-16 14:52:36 +0530

    updates

--- basics/array.cpp
+++ basics/array.cpp
@@ -9,12 +9,30 @@ void printArray(int array[], int size){
     cout << endl;
 }
 
+int maxmin(int arr[], int size, int &max, int &min){
+    max = arr[0];
+    min = arr[0];
+    for(int i=0; i<size; i++){
+        if(arr[i] > max){
+            max = arr[i];
+        }
+        if(arr[i] < min){
+            min = arr[i];
+        }
+    }
+    return 0;
+}
+
 int main(){
     
     int arr[17] = {0};
-    fill_n(arr, 17, 5);
+    //fill_n(arr, 17, 5);
     //cout << sizeof(arr)/sizeof(int) << endl;
     //cout << arr[6] << endl;
     printArray(arr, sizeof(arr)/sizeof(int));
+    int arr1[17] = {100,5,9,134, 57, 67, 89, 34, 23, 45, 67, 89, 90, 23, 45, 67, -87};
+    int max,min;
+    maxmin(arr1, 17, max, min);
+    cout << "Max: " << max << "   "<< "Min: "<< min<<  endl;
     return 0;
 }
