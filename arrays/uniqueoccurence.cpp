9b0ed615f8e0d2ffcbb5e790f53bed8c6df57136
shankerabhigyan <shankerabhigyan@gmail.com>
2023-04-19 20:51:32 +0530

    updates

---
+++ arrays/uniqueoccurence.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+//check if an array has unique number of occurences for each number.
+//length constrain = 1000
+
+bool uniqueOccurence(int arr[]){
+    return true;
+}
+
+
+
+int main(){
+    int arr[1000] = {0};
+    vector <int>& v; // vector of size 1000
+    for(int i=0;i<1000;i++){
+        cin>>arr[i];
+        v.push_back(arr[i]);
+    }
+
+    return 0;
+}
