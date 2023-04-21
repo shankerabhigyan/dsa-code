2a5c1830af5fd3a4e52e6e5ca79749146a555625
shankerabhigyan <shankerabhigyan@gmail.com>
2023-04-21 17:06:07 +0530

    new files

--- arrays/binarysearch.cpp
+++ arrays/binarysearch.cpp
@@ -1,25 +1,40 @@
 #include<iostream>
 using namespace std;
 
-int binarySearch(int arr[], int size){
-    return 0;
+int binarySearch(int arr[], int size, int element){
+    int start = 0;
+    int end = size-1;
+    while(start<=end)
+    {
+        int mid = (start+end)/2;
+        if(arr[mid]==element){
+            return mid;
+        }
+        else if(arr[mid]>element){
+            end = mid-1;
+        }
+        else{
+            start = mid+1;
+        }
+    }
+    return 404;
 }
 
 int main(){
-    cout >> "Input array size";
+    cout << "Input array size" << endl;
     int size;
-    cin << size;
+    cin >> size;
     int arr[size];
-    cout >> "Input array elements";
+    cout <<  "Input array elements in increasing"<< endl;
     for(int i=0; i<size; i++){
-        cin << arr[i];
+        cin >> arr[i];
     }
-    cout >> "Input element to search";
-    int element;
-    cin << element;
+    cout << "Input element to search"<< endl;
+    int element=0;
+    cin >> element;
     int index = binarySearch(arr, size, element);
 
-    cout<< "Element found at index: " << index;
+    cout<< "Element found at index: " << index << endl;
 
     return 0;
 }
