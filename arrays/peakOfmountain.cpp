f4d373e75a04c06e87c91f0edc0106270071595d
shankerabhigyan <shankerabhigyan@gmail.com>
2023-04-22 10:05:14 +0530

    update

--- arrays/peakOfmountain.cpp
+++ arrays/peakOfmountain.cpp
@@ -6,12 +6,36 @@
 using namespace std;
 
 int peak(int arr[], int size){
-    
+    int start = 0;
+    int end = size-1;
+    int mid = start + (end-start)/2;
+
+    while(! arr[mid]>arr[mid-1] && ! arr[mid]>arr[mid+1]){
+        mid = start + (end-start)/2;
+        if(arr[mid]>arr[mid-1]){
+            start = mid+1;
+        }
+        else{
+            end = mid-1;
+        }
+
+    }
+    return mid;
 }
 
 
 int main(){
 
+    int size = 0;
+    cout << "Input array size" << endl;
+    cin >> size;
+    int arr[size];
+    cout <<  "Input array elements in mountain pattern"<< endl;
+    for(int i=0; i<size; i++){
+        cin >> arr[i];
+    }
+    int peakIndex = peak(arr, size);
+    cout << "Peak index: " << peakIndex << endl;
 
     return 0;
 }
