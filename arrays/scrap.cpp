373732dd6c717d8ee5ff0f7219d84d3b25118262
shankerabhigyan <shankerabhigyan@gmail.com>
2023-04-22 09:49:02 +0530

    new question

---
+++ arrays/scrap.cpp
@@ -0,0 +1,11 @@
+//rough code
+#include<iostream>
+using namespace std;
+
+int main(){
+    vector<int> v;
+    v.assign(2,1);
+    int size = v.size();
+    cout << "Size of vector: " << size << endl;
+    return 0;
+}
