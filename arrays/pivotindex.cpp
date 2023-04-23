82b9e3bd1ac14eeae9c1b42ac0844acd2883b9a4
shankerabhigyan <shankerabhigyan@gmail.com>
2023-04-23 09:54:43 +0530

    pivot index

---
+++ arrays/pivotindex.cpp
@@ -0,0 +1,12 @@
+// find the pivot index of an array
+// https://leetcode.com/problems/find-pivot-index/
+// pivot index is the index where the sum of elements to the left and right are equal
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+
+    return 0;
+}
