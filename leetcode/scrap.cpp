afb26f9c266d4509e0809d5fce0ad50fe64e5bc6
shankerabhigyan <shankerabhigyan@gmail.com>
2023-05-25 20:23:01 +0530

    problem

--- leetcode/scrap.cpp
+++ leetcode/scrap.cpp
@@ -1,5 +1,6 @@
-// https://leetcode.com/problems/median-of-two-sorted-arrays/
+//rough code
 #include<iostream>
+#include<vector>
 using namespace std;
 
 double median(int arr1[], int arr2[], int n1, int n2){
@@ -34,23 +35,15 @@ double median(int arr1[], int arr2[], int n1, int n2){
 }
 
 int main(){
-    int n1,n2;
-    cout << "Input size of both arrays" << endl;
-    cin >> n1 >> n2;
-    int arr1[n1], arr2[n2];
-    cout << "Input elements of array 1" << endl;
-    for(int i=0; i<n1; i++){
-        cin >> arr1[i];
-    }
-    cout << "Input elements of array 2" << endl;
-    for(int i=0; i<n2; i++){
-        cin >> arr2[i];
-    }
-    //double med =  median(arr1,arr2,n1,n2);
-    cout << "Median of both arrays is: " << median(arr1,arr2,n1,n2) << endl;
-    //check data type of med
-    //cout << "Data type of med: " << typeid(median(arr1,arr2,n1,n2)).name() << endl;
-
+    vector<int> v;
+    v.assign(2,1);
+    int size = v.size();
+    cout << "Size of vector: " << size << endl;
+    cout << v[1] << endl; 
+    int arr[2];
+    arr[0]=v[0];
+    arr[1]=v[1];
+    // extract a vector reference & nums to a vector p
+    // vector<int> p = nums;
     return 0;
-
 }
