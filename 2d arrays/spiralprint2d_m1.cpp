6de3bd4d08730c7a4bd319aa997d230cc9a8b9eb
shankerabhigyan <shankerabhigyan@gmail.com>
2023-06-08 13:28:51 +0530

    2d array

---
+++ 2d arrays/spiralprint2d_m1.cpp
@@ -0,0 +1,81 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> spiralPrint(vector<vector<int>> arr){
+    int n=arr.size();
+    int m=arr[0].size();
+    int rs=0, cs=0, re=n-1,ce=m-1, count=0;
+    vector<int> spiral;
+    while(count<m*n){
+        for(int i=cs;i<=ce;i++){
+            cout << arr[rs][i] << " ";
+            spiral.push_back(arr[rs][i]);
+            count++;
+        }
+        rs++;
+        if (rs>re){
+            break;
+        }
+        if (count>=m*n){
+                break;
+        }
+
+        for(int i=rs;i<=re;i++){
+            cout << arr[i][ce] << " ";
+            spiral.push_back(arr[i][ce]);
+            count++;
+        }
+        ce--;
+        if (count>=m*n){
+                break;
+        }
+        for(int i=ce;i>=cs;i--){
+            cout << arr[re][i] << " ";
+            spiral.push_back(arr[re][i]);
+            count++;
+        }
+        re--;
+        if (count>=m*n){
+                break;
+        }
+        for(int i=re;i>=rs;i--){
+            cout << arr[i][cs] << " ";
+            spiral.push_back(arr[i][cs]);
+            count++;
+        }
+        cs++;
+    }
+    cout << endl;
+    return spiral;
+    // the complexity of this function is O(n*m)
+}
+
+int main(){
+    int n,m;
+    cout << "Enter the number of rows and columns : " << endl;
+    cin >> n >> m;
+    vector<vector<int>> arr(n,vector<int>(m));
+    /* else 
+     vector<vector<int>> arr;
+     arr.resize(n);
+     for (int i=0;i<n;i++){
+         arr[i].resize(m);
+     }*/
+    cout << "Enter "<<(n*m)<< " elements of the array : " << endl;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cin >> arr[i][j];
+        }
+    }
+
+    cout << "The elements of the array are : " << endl;
+    for (auto i:arr){ // i is a vector of integers
+        for(auto j:i){ // j is an integer
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+
+    cout << "The spiral print is : ";
+    spiralPrint(arr,n,m);
+}   
