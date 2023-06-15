2a082e3e52405c9e5f1ba55a4b2f53b60ecb8bb0
shankerabhigyan <shankerabhigyan@gmail.com>
2023-06-15 18:03:23 +0530

    updated

--- recursion/fibonacci.cpp
+++ recursion/fibonacci.cpp
@@ -1,11 +1,11 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int fibonacci(int n, int f=0, int s=1){
-    if(n == 1){
-        return f;
+int fibonacci(int n){
+    if(n == 0 || n == 1){
+        return n;
     }
-    return fibonacci(n-1, s, f+s); 
+    return fibonacci(n-1) + fibonacci(n-2);
 }
 
 int main(){
