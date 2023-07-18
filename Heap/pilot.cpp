de00bacca027f3c2c592e9780766d6119c7eb551
shankerabhigyan <shankerabhigyan@gmail.com>
2023-07-18 17:30:24 +0530

    added heap

--- Heap/pilot.cpp
+++ Heap/pilot.cpp
@@ -21,8 +21,7 @@ class MaxHeap{
         if(heapSize == maxSize){
             cout << "heap overflow for int "<< val << "." << endl;
         }
-        heapSize = heapSize + 1;
-        int i = heapSize-1;
+        int i = heapSize++;
         arr[i] = val;
 
         // check heapify property
@@ -36,14 +35,46 @@ class MaxHeap{
         int l = 2*i + 1;
         int r = 2*i + 2;
         int largest = i;
+
+        if(l<heapSize && arr[l]>arr[i]){
+            largest = l;
+        }
+        if(r<heapSize && arr[r]>arr[largest]){
+            largest = r;
+        }
+        if(largest!=i){
+            swap(arr[i],arr[largest]);
+            MaxHeapify(largest);
+        }
     }
 
-    void removeMax(){
+    int removeMax(){
+        if(heapSize<=0){
+            return INT_MIN;
+        }
+        if(heapSize==1){
+            heapSize--;
+            return arr[0];
+        }
 
+        int root = arr[0];
+        arr[0] = arr[heapSize-1];
+        heapSize--;
+        MaxHeapify(0);
+        return root;
+    }
+
+    void increaseKey(int i, int val){
+        arr[i] = val;
+        while(i>0 && arr[getParent(i)]<arr[i]){
+            swap(arr[getParent(i)],arr[i]);
+            i = getParent(i);
+        }
     }
 
     void deletekey(int i){
-
+        increaseKey(i, INT_MAX);
+        removeMax();
     }
 
     void display(){
@@ -56,12 +87,20 @@ class MaxHeap{
 
 int main(){
     MaxHeap heap(10);
-    heap.insert(1);
-    heap.insert(2);
-    heap.insert(5);
-    heap.insert(3);
-    heap.insert(4);
-    heap.insert(10);
+    int x;
+    cout << "Enter elements of the heap, (terminates @ -1)" << endl; 
+    cin >> x;
+    while(x!=-1){
+        heap.insert(x);
+        cin >> x;
+    }
+    cout << "initial heap..." << endl;
+    heap.display();
+    
+    cout << endl << "enter index to be deleted : ";
+    cin >> x;
+    heap.deletekey(x);
+    cout << "new heap..." << endl;
     heap.display();
 
     return 0;
