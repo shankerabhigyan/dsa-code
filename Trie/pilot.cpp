57e336059b9890c30575bad79fb13016e219c6f4
shankerabhigyan <shankerabhigyan@gmail.com>
2023-07-27 23:25:25 +0530

    updated Trie delete key funct

--- Trie/pilot.cpp
+++ Trie/pilot.cpp
@@ -36,6 +36,58 @@ struct TrieNode{
         }
         return curr->eow;
     }
+
+    void deleteKey(string &key){
+        TrieNode* curr = this;
+        TrieNode* lastBranchNode = NULL;
+        char lastBranchChar;
+        for(auto ch:key){
+            int index = ch-'a';
+            if(!curr->child[index]){
+                cout << "Key : \'" << key << "\' does not exist." << endl;
+                break;
+            }
+            int count = 0;
+            for(int i=0;i<VOCAB_SIZE;i++){
+                if(curr->child[i]){
+                    count++;
+                }
+            }
+            if(count > 1){
+                lastBranchNode = curr;
+                lastBranchChar = ch;
+            }
+            curr = curr->child[index];
+        }
+
+        if(!curr->eow){
+            cout << "Key : \'" << key << "\' does not exist." << endl;
+        }
+        else{
+            // we need to account for three cases
+            // 1. curr is not a leaf node and has children, i.e. it is a prefix of some other word
+            // 2. curr shares a prefix with some other word
+            // 3. curr is a leaf node and has no children neither does it share a prefix with some other word
+            int count = 0;
+            for(int i=0;i<VOCAB_SIZE;i++){
+                if(curr->child[i]){
+                    count++;
+                }
+            }
+
+            if(count > 0){
+                curr->eow = false;
+            }
+            else{
+                if(lastBranchNode){
+                    lastBranchNode->child[lastBranchChar-'a'] = NULL;
+                }
+                else{
+                    delete this->child[key[0]-'a'];
+                }
+            }
+        }
+    }
 };
 
 int main(){
@@ -46,6 +98,14 @@ int main(){
     }
 
     string key;
+    cout << "Enter the key to search: ";
+    cin >> key;
+    cout << '\'' << key << '\'' << " is " << (root->searchKey(key) ? "present" : "not present") << endl;
+
+    cout << "Enter the key to delete: ";
+    cin >> key;
+    root->deleteKey(key);
+
     cout << "Enter the key to search: ";
     cin >> key;
     cout << '\'' << key << '\'' << " is " << (root->searchKey(key) ? "present" : "not present") << endl;
