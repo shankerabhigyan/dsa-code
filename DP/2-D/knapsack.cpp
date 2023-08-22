6b385334de51c43f95e615509fe032891294776d
shankerabhigyan <shankerabhigyan@gmail.com>
2023-08-22 15:00:56 +0530

    added 0/1 knapsack

--- DP/2-D/knapsack.cpp
+++ DP/2-D/knapsack.cpp
@@ -1,41 +1,129 @@
 #include<bits/stdc++.h>
 using namespace std;
 //https://www.codingninjas.com/studio/problems/0-1-knapsack_920542?topList=love-babbar-dsa-sheet-problems&leftPanelTab=0&utm_source=youtube&utm_medium=affiliate&utm_campaign=Lovebabbar
+//https://www.youtube.com/watch?v=xdPv2SZJLVI&list=PLDzeHZWIZsTryvtXdMr6rPh4IDexB5NIA&index=121&ab_channel=CodeHelp-byBabbar
 
-// recursive solution, suboptimal, gives TLE
-int solve(vector<int> &weight, vector<int> &value, int w, int val, int &n, int &maxWeight,vector<int> visited){
-    if(w==maxWeight){
-        return val;
-    }
-    if(w>maxWeight){
-        return INT_MIN;
-    }
-    int ans = INT_MIN;
-    bool flag = false;
-    for(int i=0;i<n;i++){
-        if(!visited[i]){
-            visited[i] = 1;
-            int x = solve(weight,value,w+weight[i],val+value[i],n,maxWeight,visited);
-            visited[i] = 0;
-            if(x==INT_MIN){
-                x = val;
-            }
-            ans = max(ans,x);
-            flag = true;
+// recursive solution
+int solve(vector<int> &weight, vector<int> &value, int index, int capacity){
+    if(index==0){
+        if(weight[index]<=capacity){
+            return value[0];
+        }
+        else{
+            return 0;
         }
     }
-    if(!flag){ // if all the elements are visited then return the current value
-        return val;
+    int include = 0;
+    if(weight[index]<=capacity){
+        include = value[index] + solve(weight,value,index-1,capacity-weight[index]);
     }
+    int exclude = solve(weight,value,index-1,capacity);
+    int ans = max(include,exclude);
     return ans;
 }
 
-int knapsack(vector<int> weight, vector<int> value, int n, int maxWeight){
-    vector<int> visited(n,0);
-    int ans = solve(weight,value,0,0,n,maxWeight,visited);
-    if(ans==INT_MIN){
-        return 0;
+// memoization
+int solveMem(vector<int> &weight, vector<int> &value, int index, int capacity, vector<vector<int>> &dp){
+    if(index==0){
+        if(weight[index]<=capacity){
+            return value[0];
+        }
+        else{
+            return 0;
+        }
     }
+    if(dp[index][capacity]!=-1){
+        return dp[index][capacity];
+    }
+    int include = 0;
+    if(weight[index]<=capacity){
+        include = value[index] + solveMem(weight,value,index-1,capacity-weight[index],dp);
+    }
+    int exclude = solveMem(weight,value,index-1,capacity,dp);
+    dp[index][capacity] = max(include,exclude);
+    return dp[index][capacity]; 
+}
+
+// Tabulation
+int solveTab(vector<int> weight, vector<int> value, int n, int maxWeight){
+    vector<vector<int>> dp(n,vector<int>(maxWeight+1,0));
+    int capacity = maxWeight;
+    for(int w=weight[0];w<=capacity;w++){
+        if(weight[0]<=capacity){
+            dp[0][w] = value[0];
+        }
+        else{
+            dp[0][w] = 0;
+        }
+    }
+    for(int i=1;i<n;i++){
+        for(int w=0;w<=capacity;w++){
+            int include = 0;
+            if(weight[i]<=w){
+                include = value[i] + dp[i-1][w-weight[i]];
+            }
+            int exclude = dp[i-1][w];
+            dp[i][w] = max(include,exclude);
+        }
+    }
+    return dp[n-1][capacity];
+}
+
+// space optimisation
+int solveOpt(vector<int> weight, vector<int> value, int n, int maxWeight){
+    vector<int> curr(maxWeight+1,0);
+    vector<int> prev(maxWeight+1,0);
+    int capacity = maxWeight;
+    for(int w=weight[0];w<=capacity;w++){
+        if(weight[0]<=capacity){
+            prev[w] = value[0];
+        }
+        else{
+            prev[w] = 0;
+        }
+    }
+    for(int i=1;i<n;i++){
+        for(int w=0;w<=capacity;w++){
+            int include = 0;
+            if(weight[i]<=w){
+                include = value[i] + prev[w-weight[i]];
+            }
+            int exclude = prev[w];
+            curr[w] = max(include,exclude);
+        }
+        prev = curr;
+    }
+    return prev[capacity];
+}
+
+// further optimisation
+int solveOpt2(vector<int> weight, vector<int> value, int n, int maxWeight){
+    vector<int> curr(maxWeight+1,0);
+    int capacity = maxWeight;
+    for(int w=weight[0];w<=capacity;w++){
+        if(weight[0]<=capacity){
+            curr[w] = value[0];
+        }
+        else{
+            curr[w] = 0;
+        }
+    }
+    for(int i=1;i<n;i++){
+        for(int w=capacity;w>=0;w--){
+            int include = 0;
+            if(weight[i]<=w){
+                include = value[i] + curr[w-weight[i]];
+            }
+            int exclude = curr[w];
+            curr[w] = max(include,exclude);
+        }
+    }
+    return curr[capacity];
+}
+
+int knapsack(vector<int> weight, vector<int> value, int n, int maxWeight){
+    vector<vector<int>> dp(n,vector<int>(maxWeight+1,-1));
+    int ans = solveMem(weight,value,n-1,maxWeight,dp);
     return ans;
 }
 
